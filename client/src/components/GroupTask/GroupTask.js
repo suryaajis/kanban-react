0ec383712a9acbd9ffc38065faefce92f6c14026
suryaajis330 <suryaajis330@gmail.com>
2023-01-09 06:37:01 +0700

    modal add task

--- client/src/components/GroupTask/GroupTask.js
+++ client/src/components/GroupTask/GroupTask.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./GroupTask.css";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { Modal } from "../Modal/Modal";
 
 export const GroupTask = (props) => {
   const { id, months } = props.section;
+
+  const [showModal, setShowModal] = useState(false);
+
+  const addTask = (event) => {
+    event.preventDefault();
+    console.log("add task");
+  };
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
 
   return (
     <div className={`group-container ${"task-" + id}`}>
@@ -10,11 +23,33 @@ export const GroupTask = (props) => {
         <p className="badge-text">Group Task {id}</p>
       </div>
       <p className="month">{months}</p>
-
       {props.children}
-      <div>
-        <div></div>
-        <p>New task</p>
+
+      <Modal show={showModal}>
+        <div className="head">
+          <h3>Create Task</h3>
+        </div>
+        <div className="body">
+          <form onSubmit={addTask}>
+            <label>
+              <p>Task name</p>
+              <input id="name" name="name" />
+            </label>
+            <label>
+              <p>Progress</p>
+              <input id="progress" name="progress" />
+            </label>
+          </form>
+        </div>
+        <div className="foot">
+          <button className="modal-btn" onClick={handleClose}>Cancel</button>
+          <button className="modal-btn success-btn">Save Task</button>
+        </div>
+      </Modal>
+
+      <div className="btn-task" onClick={() => setShowModal(!showModal)}>
+        <IoMdAddCircleOutline />
+        <p className="btn-text">New task</p>
       </div>
     </div>
   );
---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Modal.css';
+
+export const Modal = ({ handleClose, show, children }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        {children}
+      </section>
+    </div>
+  );
+};
